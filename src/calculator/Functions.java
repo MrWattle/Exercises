2de12ab4d8b1c4e4ec1b081926daa7e7813f6db1
MrWattle <rix.ukr@gmail.com>
2015-12-12 18:14:39 +0200

    Simple calculator. It reads an expressions such as: x1 (OP) x2, where: (OP) - one of the next operations + - * /

--- src/calculator/Functions.java
+++ src/calculator/Functions.java
@@ -11,22 +11,37 @@ public class Functions {
         }
     }
 
+    class Sub implements Operation{
+        public double function (double x, double y ){
+            return x - y;
+        }
+    }
+
+    class Mul implements Operation{
+        public double function (double x, double y ){
+            return x * y;
+        }
+    }
+
+    class Div implements Operation{
+        public double function (double x, double y ){
+            return x / y;
+        }
+    }
+
     public Operation detOfOperation (String s){
         if (s.equals("+") )
             return new Add();
+        else if (s.equals("-") )
+            return new Sub();
+        else if (s.equals("*") )
+            return new Mul();
+        else if (s.equals("/") )
+            return new Div();
         else
             return null;
     }
-}
 
-/*
-    class DetermOfOperation{
-        String s;
-        Operation op;
-        DetermOfOperation (String s){
-            this.s = s;
-            if (s.equals("+") )
-                op = new Add();
-        }
+    class Priority{
     }
-*/
+}
